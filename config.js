638e4babfc7e10d35ed9b5abed2b28ce42761cfc
Phil Craig <philipcraig@carfax.com>
2022-02-27 10:42:09 -0500

    updated subgraph config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0xbeD4eE795fD00BcfdA08b88015DD3fD7b2521fD1"
+  export const contractAddress = "0xe35953f00dcF8C922645c7ffEa8ac8CEdfF20028"
   export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
   
